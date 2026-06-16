f97c411d0464b7821e6a0d9c37137a4843a886d4
zelttikDevv <alberrttoop@gmail.com>
2026-06-16 17:01:44 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
-};
-export default config;
+}
